72ba83fee15129867943d0c57346f0e0e68a2e68
narendra93900 <narendrasaigarlapati@lyrostech.com>
2024-09-19 13:58:28 +0530

    uploding changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Landing from './landingPage/Landing';
-import Login from './loginPage/Login';
+import Landing from './pages/landingPage/Landing';
+import Login from './pages/loginPage/Login';
+import Register from './pages/register/Register';
+import Dashboard from './pages/dashboard/AdminDashboard';
+import CreateQuiz from './pages/createQuiz/CreateQuiz';
+import AdminDashboard from './pages/dashboard/AdminDashboard';
+import UserDashboard from './pages/dashboard/UserDashboard';
+import QuizDetails from './pages/createQuiz/QuizDetails';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />  {/* Landing page */}
-        <Route path="/login" element={<Login />} /> {/* Login page */}
+        <Route path="/" element={<Landing />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/admin-dashboard" element={<AdminDashboard/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/createquiz" element={<CreateQuiz/>}/>
+        <Route path="/userdashboard" element={<UserDashboard/>}/>
+        <Route path="/quizdetails" element={<QuizDetails/>}/>
+        
       </Routes>
     </Router>
   );
